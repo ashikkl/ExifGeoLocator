04b666a43c534c0f8cd8cf3ddfed374ee989ae06
Ashik K L <ashikkl19112001@gmail.com>
2023-02-11 01:23:37 +0530

    minor fixes

--- exif-geo-locator/src/components/Add.jsx
+++ exif-geo-locator/src/components/Add.jsx
@@ -1,15 +1,26 @@
 import "../scss/Add.scss";
 import { FaPlus } from "react-icons/fa";
 
-function toggleBar (){
-    let elem= document.getElementById('uploadBar');
-    elem.classList.add('visible')
-    console.log('5');
+function toggleBar() {
+  let elem = document.getElementById("uploadBar");
+  if (elem.classList.contains("hidden")) {
+    elem.classList.add("visible");
+    elem.classList.remove("hidden");
+  }
+  else{elem.classList.add("hidden");
+elem.classList.remove("visible");}
+  if (document.getElementById("toggleButton").classList.contains("inactive")) {
+    document.getElementById("toggleButton").classList.add("active");
+    document.getElementById("toggleButton").classList.remove("inactive");
+  } else {
+    document.getElementById("toggleButton").classList.add("inactive");
+    document.getElementById("toggleButton").classList.remove("active");
+  }
 }
 
 function Add() {
   return (
-    <button id="toggleButton " className="addButton" onClick={toggleBar}>
+    <button id="toggleButton" className="addButton inactive" onClick={toggleBar}>
       <FaPlus />
     </button>
   );

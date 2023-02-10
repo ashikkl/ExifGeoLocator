87796655045db47e9b1e6022e9d5adece5976575
Ashik K L <ashikkl19112001@gmail.com>
2023-02-10 19:07:18 +0530

    add footer, header and scroll

---
+++ exif-geo-locator/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "../scss/Footer.scss";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <div className="text prevent-select">
+        <div> &copy;{Date().split(` `)[3]} Ashik K L</div>
+        <div>All rights reserved.</div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ exif-geo-locator/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../scss/Header.scss'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="head prevent-select">ExifGeoLocator</div>
+    </div>
+  );
+}
+
+
+export default Header;

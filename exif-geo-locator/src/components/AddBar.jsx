2ca55aa747ddb0fdae4357c8bcb2f071a606c696
Ashik K L <ashikkl19112001@gmail.com>
2023-03-03 21:26:15 +0530

    refactoring

--- exif-geo-locator/src/components/AddBar.jsx
+++ exif-geo-locator/src/components/AddBar.jsx
@@ -4,15 +4,9 @@ import React from "react";
 
 function AddBar() {
   const [loading, setIsLoading] = React.useState(false);
-  let reload = () => {
-    setIsLoading(false);
-    window.location.reload(false);
-  };
 
   function handleChange(files) {
-
     async function compressImage(blobImg, percent) {
-
       let bitmap = await createImageBitmap(blobImg);
       let canvas = document.createElement("canvas");
       let ctx = canvas.getContext("2d");
@@ -24,64 +18,68 @@ function AddBar() {
     }
 
     setIsLoading(true);
-      for (let i = 0; i < files.length; i++) {
-        let file = files[i];
-        
-        if (file && file.name) {
-          EXIF.getData(file, function () {
-            var exifData = EXIF.pretty(this);
-            if (exifData) {
-              if (EXIF.getTag(this, "GPSLatitude")) {
-                let lat = EXIF.getTag(this, "GPSLatitude");
-                let latref = EXIF.getTag(this, "GPSLatitudeRef");
-                let lon = EXIF.getTag(this, "GPSLongitude");
-                let lonref = EXIF.getTag(this, "GPSLongitudeRef");
+    for (let i = 0; i < files.length; i++) {
+      let file = files[i];
 
-                let latitude = "";
-                let longitude = "";
+      if (file && file.name) {
+        EXIF.getData(file, function () {
+          var exifData = EXIF.pretty(this);
+          if (exifData) {
+            if (EXIF.getTag(this, "GPSLatitude")) {
+              let lat = EXIF.getTag(this, "GPSLatitude");
+              let latref = EXIF.getTag(this, "GPSLatitudeRef");
+              let lon = EXIF.getTag(this, "GPSLongitude");
+              let lonref = EXIF.getTag(this, "GPSLongitudeRef");
 
-                latitude = `${lat[0]} ${lat[1]} ${lat[2]} ${latref}`;
-                longitude = `${lon[0]} ${lon[1]} ${lon[2]} ${lonref}`;
+              let latitude = "";
+              let longitude = "";
 
-                var myDat = localStorage["data"];
-                var stored = localStorage["data"];
-                if (stored) myDat = JSON.parse(stored);
-                else myDat = [];
-                const reader = new FileReader();
+              latitude = `${lat[0]} ${lat[1]} ${lat[2]} ${latref}`;
+              longitude = `${lon[0]} ${lon[1]} ${lon[2]} ${lonref}`;
 
-                reader.readAsDataURL(file);
-                reader.addEventListener("load", () => {
-                  const cImg = async () => {
-                    const a = await compressImage(file, 75);
+              var myDat = localStorage["data"];
+              var stored = localStorage["data"];
+              if (stored) myDat = JSON.parse(stored);
+              else myDat = [];
+              const reader = new FileReader();
 
-                    const data = {
-                      id: myDat.length + 1,
-                      lat: latitude,
-                      long: longitude,
-                      file: a,
-                      fileName: file.name,
-                    };
+              reader.readAsDataURL(file);
+              reader.addEventListener("load", () => {
+                const cImg = async () => {
+                  const a = await compressImage(file, 75);
 
-                    try {
-                      localStorage.setItem(
-                        "data",
-                        JSON.stringify([...myDat, data])
-                      );
-                    } catch (error) {
-                      alert("Local Storage Full " + error);
-                    }
+                  const data = {
+                    id: myDat.length + 1,
+                    lat: latitude,
+                    long: longitude,
+                    file: a,
+                    fileName: file.name,
                   };
-                  cImg();
-                });
-              } else {
-                alert("No Location Data Found in '" + file.name + "'.");
-              }
+
+                  try {
+                    localStorage.setItem(
+                      "data",
+                      JSON.stringify([...myDat, data])
+                    );
+                    if (files.length === i + 1) {
+                      setIsLoading(false);
+                      window.location.reload(false);
+                    }
+                  } catch (error) {
+                    alert("Local Storage Full " + error);
+                  }
+                };
+                cImg();
+              });
             } else {
-              alert("No EXIF data found in image '" + file.name + "'.");
+              alert("No Location Data Found in '" + file.name + "'.");
             }
-          });
-        }
+          } else {
+            alert("No EXIF data found in image '" + file.name + "'.");
+          }
+        });
       }
+    }
   }
 
   // handle drag events
